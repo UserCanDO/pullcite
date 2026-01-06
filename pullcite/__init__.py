55c0dd359ccc39644a54f2d4c67f68e3235a88a1
Dike Kalu <dike@dikekalu.com>
2026-01-06 09:46:42 -0500

    Bump version to 0.2.3

--- pullcite/__init__.py
+++ pullcite/__init__.py
@@ -46,7 +46,7 @@ Example:
     >>> print(evidence.bbox)           # Bounding box coordinates
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 # Core types
 from .core.document import Document
